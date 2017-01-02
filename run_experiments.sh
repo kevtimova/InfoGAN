a72ab93eadbfd4955725067fd2b81c20da34a08f
Andrew Drozdov <andrew@mrdrozdov.com>
2017-01-01 19:12:22 -0500

    Fix noise param

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,12 +4,12 @@ module load cuda-7.5
 module load cudnn-v4-cuda7
 
 PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line --dataset LINE
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line-fg --dataset LINE --noise fg
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line-bg --dataset LINE --noise bg
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line-both --dataset LINE --noise both
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line-fg --dataset LINE --noise FG
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line-bg --dataset LINE --noise BG
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name line-both --dataset LINE --noise BOTH
 
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects --dataset RECTS 
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects-fg --dataset RECTS --noise fg
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects-bg --dataset RECTS --noise bg
-PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects-both --dataset RECTS --noise both
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects --dataset RECTS
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects-fg --dataset RECTS --noise FG
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects-bg --dataset RECTS --noise BG
+PYTHONPATH='.' python launchers/run_mnist_exp.py --experiment_name rects-both --dataset RECTS --noise BOTH
 
